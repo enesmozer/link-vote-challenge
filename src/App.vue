f22f6fda96d649fa2163610f1fa3ded12cfdbfc7
Enes Malik Özer <enesmozer@gmail.com>
2021-09-19 00:34:45 +0300

    header implemented

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header logo="src/assets/logo.png"></Header>
     <router-view />
   </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<script>
+import Logo from "../assets/logo.svg";
+export default {
+  name: "Header",
+  props: {
+    logo: String,
+  },
+  components: {
+    Logo,
+  },
+};
+</script>
+<template>
+  <header class="header">
+    <div class="header-main">
+      <Logo class="header-logo" />
+      <div class="header-title">
+        <b>Link</b><span>Vote</span
+        ><span style="font-weight: 500">Challange</span>
+      </div>
+    </div>
+    <div class="divider"></div>
+  </header>
+</template>
+<style scoped lang="scss">
+.header {
+  @apply w-11/12;
+  @apply h-28;
+  @apply relative;
+  @apply flex;
+  @apply flex-col;
+  @apply justify-center;
+  @apply absolute;
+  @apply left-1/2;
+  @apply transform;
+  @apply -translate-x-1/2;
+  &-main {
+    @apply flex;
+    @apply items-center;
+  }
+  &-title {
+    @apply absolute;
+    @apply right-0;
+    @apply text-2xl;
+  }
+  .divider {
+    @apply w-full;
+    @apply h-px;
+    @apply bg-gray-400;
+    @apply mt-3;
+  }
+}
+</style>
