2a7e0a51c0dfe783476a725c0cdd7f23673bbcc3
Enes Malik Özer <enesmozer@gmail.com>
2021-09-20 19:44:18 +0300

    notify implemented to main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import "./assets/styles/app.scss";
+import Notifications from "vue-notification";
 
 Vue.config.productionTip = false;
+Vue.use(Notifications);
 
 new Vue({
   router,
