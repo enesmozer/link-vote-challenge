c9894ce4c7826340825c114dda0ac832002e0043
Enes Malik Özer <enesmozer@gmail.com>
2021-09-20 23:23:45 +0300

    added more tests

--- tests/unit/LinkCard.spec.js
+++ tests/unit/LinkCard.spec.js
@@ -1,16 +1,21 @@
 import Vue from "vue";
-import LinkCard from "@/components/LinkCard";
 import { expect } from "chai";
+import sinon from "sinon";
+import LinkCard from "@/components/LinkCard";
 
 describe("LinkCard.vue", () => {
-  it("should link links", () => {
+  let vm = null;
+  let spy = null;
+  let name = "";
+  let url = "";
+  let point = 0;
+  beforeEach(() => {
     const container = document.createElement("div");
     const linkCardComponent = Vue.extend(LinkCard);
-    const name = "Vue.js";
-    const url = "(https://vuejs.org/)";
-    const point = 5;
-
-    const vm = new linkCardComponent({
+    name = "Vue.js";
+    url = "(https://vuejs.org/)";
+    point = 5;
+    vm = new linkCardComponent({
       propsData: {
         name,
         url,
@@ -18,10 +23,12 @@ describe("LinkCard.vue", () => {
       },
     });
     vm.$mount(container);
-
+    spy = sinon.spy(vm, "$emit");
+  });
+  it("should link links", () => {
     expect(vm.name).contains(name);
     expect(vm.url).contains(url);
-    expect(vm.point).equal(5);
+    expect(vm.point).equal(point);
     expect(vm.$el.querySelector(".link-card-info-name").textContent).equal(
       name
     );
@@ -29,5 +36,23 @@ describe("LinkCard.vue", () => {
     expect(
       Number(vm.$el.querySelector(".link-card-points .point").textContent)
     ).equal(point);
+  });
+  it("should emit event when delete clicked", () => {
+    vm.$el.querySelector(".link-card-delete").click();
+    expect(spy.called).equal(true);
+    const [eventName] = spy.args[0];
+    expect(eventName).equal("delete");
+  });
+  it("should emit event when up vote clicked", () => {
+    vm.$el.querySelector(".vote.up").click();
+    expect(spy.called).equal(true);
+    const [eventName] = spy.args[0];
+    expect(eventName).equal("upVote");
+  });
+  it("should emit event when down vote clicked", () => {
+    vm.$el.querySelector(".vote.down").click();
+    expect(spy.called).equal(true);
+    const [eventName] = spy.args[0];
+    expect(eventName).equal("downVote");
   });
 });
